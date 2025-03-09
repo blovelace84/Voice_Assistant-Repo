34c8b9ea0d6c35697fde545dc7f9e4b1b500f7df
Brittani Lovelace <blovelace84@gmail.com>
2025-03-09 06:01:10 -0400

    Debugging some issues with speech_hander.py

--- core/speech_hander.py
+++ core/speech_hander.py
@@ -4,9 +4,12 @@ import pyttsx3
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
 
+
 def speak(text):
     engine.say(text)
     engine.runAndWait()
+
+
 def listen():
     with sr.Microphone() as source:
         print("Listening...")
@@ -17,4 +20,7 @@ def listen():
             return text.lower()
         except sr.UnknownValueError:
             print("Sorry, I didn't catch that.")
+            return ""
+        except sr.RequestError:
+            print("Sorry, there was an error connecting to the service.")
             return ""
